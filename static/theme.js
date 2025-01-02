0d2967600a45f411447702bea3687afa6f3e586e
James Kerr <disconsented@disconsented.com>
2025-01-03 12:14:08 +1300

    fix: Themes missing the forward slash when toggled

--- static/theme.js
+++ static/theme.js
@@ -2,5 +2,5 @@ function set_theme(e){
     localStorage.setItem("theme", e);
     document.getElementById("dark-theme").classList.toggle("is-hidden");
     document.getElementById("light-theme").classList.toggle("is-hidden");
-    document.getElementById("theme").href = e+".css";
+    document.getElementById("theme").href = "/"+e+".css";
 }
